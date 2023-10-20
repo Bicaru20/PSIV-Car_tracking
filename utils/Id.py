9d4e35192fb2fd5615302ed76331e608b176998c
Sergi Garriga <garriga2002@gmail.com>
2023-10-20 13:22:38 +0200

    Updated ID

--- utils/Id.py
+++ utils/Id.py
@@ -4,19 +4,20 @@ import uuid
 
 
 class Id:
-    def __init__(self, id, x, y):
-        self.id = uuid.uuid4()
+    def __init__(self, x, y):
+        self.id = uuid.uuid4()[:4]
         self.x = x
         self.y = y
-        self.last_update = 0
         self.first_cor = (x, y)
-        self.last_cor = (x, y)
+        self.path = []
+        self.last_cor = None
+        self.frames_not_updated = 0
 
     def update_centroid(self, x, y):
         self.x = x
         self.y = y
-        self.last_cor = (x, y)
-        self.last_update = 0
+        self.path.append((x, y))
+        self.frames_not_updated = 0
 
-    def increment_last_update(self):
-        self.last_update += 1
+    def increment_not_updated(self):
+        self.frames_not_updated += 1
