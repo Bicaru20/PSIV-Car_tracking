6a521d121dde846208c19a5f48681f5145766236
Gabriel Gausachs <g4a4b4r4i@gmail.com>
2023-10-20 10:21:07 +0200

    correct import

--- utils/Tracker.py
+++ utils/Tracker.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-from utils.Tracker_helper import Tracker
+from Tracker_helper import Tracker
 
 track = Tracker()
 
